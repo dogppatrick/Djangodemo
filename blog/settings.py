0b9684f5f5290ec3aad6adfe9b7587d8b1b4ef28
dogppatrick <dogppatrick@msn.com>
2021-07-12 12:59:47 +0800

    edit setting

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://djangodemowithmap.herokuapp.com/']
+ALLOWED_HOSTS = ['djangodemowithmap.herokuapp.com']
 
 
 # Application definition
